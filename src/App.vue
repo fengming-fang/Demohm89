7cac6e2098c2da2944abc1d5f73d844a9aff9322
fengming <1144753090@qq.com>
2019-12-20 21:36:44 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+<router-link>HOME</router-link>
+
+<!-- 一级容器 -->
+<router-view></router-view>
+
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+我是主页
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {}
+}
+</script>
